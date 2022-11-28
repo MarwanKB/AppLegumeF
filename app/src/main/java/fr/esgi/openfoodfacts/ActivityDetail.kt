4a91b4ef18b808d2e485a91a08cd00179ec89461
Shan AZIZ <shan.azizh@gmail.com>
2022-11-28 11:56:55 +0100

    new detail activity with main placeholder.jpg image

--- app/src/main/java/fr/esgi/openfoodfacts/ActivityDetail.kt
+++ app/src/main/java/fr/esgi/openfoodfacts/ActivityDetail.kt
@@ -2,12 +2,10 @@ package fr.esgi.openfoodfacts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.content.ContextCompat
 
-class MainActivity : AppCompatActivity() {
+class ActivityDetail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
-        supportActionBar?.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.action_bar))
     }
 }
